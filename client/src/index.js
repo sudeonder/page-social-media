53c4fca58b35ca1d110c3428ab7b0f2e60055b7c
Sude Önder <sudeonder999@gmail.com>
2024-05-31 21:15:24 +0300

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// this file connects the App component to the root element in the HTML file
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+// Create a root and render the App component
+const root = createRoot(rootElement);
+root.render(
+  <App/>
+);
