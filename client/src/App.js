128b0e627e46ac8fc37e6641a10ae2a903053421
Sude Önder <sudeonder999@gmail.com>
2024-06-03 20:17:41 +0300

    styledEngineProvider to change css injection order

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,45 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material/";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import "./styles.css";
+import logo from "./images/origami.png";
+
+// Most CSS-in-JS solutions inject their styles at the bottom of the HTML <head>,
+// which gives MUI precedence over your custom styles. To remove the need for !important,
+// change the CSS injection order by using the StyledEngineProvider component.
 
 const App = () => {
   return (
-    <Container maxWidth="lg">
-      <AppBar className="appBar" position="static" color="inherit">
-        <Typography className="heading" variant="h2" align="center">
-          Pages
-        </Typography>
-        <img className="image" src="pages" alt="pages_logo" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts />
+    <StyledEngineProvider injectFirst>
+      <Container maxWidth="lg">
+        <AppBar className="appBar" position="static" color="inherit">
+          <Typography className="heading" variant="h2" align="center">
+            Pages
+          </Typography>
+          <img className="logo" src={logo} alt="pages_logo" height="60" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </StyledEngineProvider>
   );
 };
 
